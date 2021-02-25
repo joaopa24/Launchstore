519bfa39952e2be56635281585c17f59120dc0fc
joknz21 <joaokn129@gmail.com>
2021-02-24 21:10:50 -0300

    Build fields , models and controllers

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ routes.get("/",function(req,res){
 })
 
 routes.get("/products/create", ProductController.create)
+routes.post("/products", ProductController.post)
+routes.get("/products/:id/edit", ProductController.edit)
 
 routes.get("/ads/create",function(req,res){
     return res.redirect("/products/create")
