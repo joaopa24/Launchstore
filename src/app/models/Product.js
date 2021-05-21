816fc983f6cc0040bfc2b408fc8b3dfc76f4d294
joknz21 <joaokn129@gmail.com>
2021-05-21 11:45:20 -0300

    category filter

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -81,7 +81,7 @@ module.exports = {
 
         if(category){
              filterQuery = `${filterQuery}
-             products.category_id = ${category_id}
+             products.category_id = ${category}
              AND 
              `
         }
